b6eab56c2eb2b498c403c880394396c192b369c6
Shamim <imsiddiquee@gmail.com>
2019-04-02 10:17:57 +0600

    apply css

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,9 +6,11 @@ class Counter extends Component {
   };
 
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <React.Fragment>
-        <span style={{ fontSize: 20 }} className="badge badge-primary m-2">
+        <span style={{ fontSize: 20 }} className={classes}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
@@ -17,7 +19,7 @@ class Counter extends Component {
   }
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? "Zero" : cclount;
+    return count === 0 ? "Zero" : count;
   }
 }
 
