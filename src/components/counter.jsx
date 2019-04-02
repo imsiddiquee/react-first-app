be287719b910ab2955c0d02ee684e98c5fe0a429
Shamim <imsiddiquee@gmail.com>
2019-04-02 10:09:57 +0600

    add css class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello world</h1> <button className="btn">Increment</button>
+        <span style={{ fontSize: 20 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : cclount;
   }
 }
 
