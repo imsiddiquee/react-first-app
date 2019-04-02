baedc34b67df00982bea1bdd783c2d5d744265b5
Shamim <imsiddiquee@gmail.com>
2019-04-02 09:24:40 +0600

    add bootstrap version npm i bootstrap@4.1.1

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello world</h1> <button className="btn">Increment</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
